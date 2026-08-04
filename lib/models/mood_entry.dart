a937439c730904e0cb253ae25dacf00d389045ac
bradpitts <48375563+bradpitts@users.noreply.github.com>
2026-08-04 19:29:09 +0530

    Update mood_entry.dart

--- lib/models/mood_entry.dart
+++ lib/models/mood_entry.dart
@@ -11,53 +11,32 @@ class MoodEntry extends HiveObject {
   final DateTime date;
 
   @HiveField(2)
-  final int primaryColorValue;
+  final Map<String, double>? moodPercentages;
 
   @HiveField(3)
-  final Map<String, double> moodPercentages;
-
-  @HiveField(4)
-  final List<String> stateTags;
-
-  @HiveField(5)
   final String? note;
 
-  @HiveField(6)
+  @HiveField(4)
   final List<String>? photoPaths;
 
-  @HiveField(7)
-  final String? strokeDataJson;
+  @HiveField(5)
+  final int primaryColorValue;
+
+  @HiveField(6)
+  final List<String>? stateTags;
 
   MoodEntry({
     required this.id,
     required this.date,
-    required this.primaryColorValue,
-    required this.moodPercentages,
-    required this.stateTags,
+    this.moodPercentages,
     this.note,
     this.photoPaths,
-    this.strokeDataJson,
-  });
-
-  MoodEntry copyWith({
-    String? id,
-    DateTime? date,
     int? primaryColorValue,
-    Map<String, double>? moodPercentages,
-    List<String>? stateTags,
-    String? note,
-    List<String>? photoPaths,
-    String? strokeDataJson,
-  }) {
-    return MoodEntry(
-      id: id ?? this.id,
-      date: date ?? this.date,
-      primaryColorValue: primaryColorValue ?? this.primaryColorValue,
-      moodPercentages: moodPercentages ?? this.moodPercentages,
-      stateTags: stateTags ?? this.stateTags,
-      note: note ?? this.note,
-      photoPaths: photoPaths ?? this.photoPaths,
-      strokeDataJson: strokeDataJson ?? this.strokeDataJson,
-    );
-  }
+    int? colorValue,
+    this.stateTags,
+  }) : primaryColorValue = primaryColorValue ?? colorValue ?? 0xFFFFD700;
+
+  int get colorValue => primaryColorValue;
+  String? get photoPath => (photoPaths != null && photoPaths!.isNotEmpty) ? photoPaths!.first : null;
+  List<String> get safePhotoPaths => photoPaths ?? (photoPath != null ? [photoPath!] : []);
 }
